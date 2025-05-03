afec99dc7ad2ed558b3cf11c5272a5676479a709
Ayan-Alvi <ayanalvi674@gmail.com>
2025-05-03 13:55:50 +0530

    First Upload

---
+++ tictacto.js
@@ -0,0 +1,15 @@
+let box = document.querySelectorAll(".cell");
+let reset = document.querySelector(".restart");
+let turnO = true;
+
+box.addEventListener("click",function(){
+    // if(turnO){
+    //     console.log("O turn");
+    //     turnO=false;
+    // }
+    // else{
+    //     box.innerText="X";
+    //     trunO=false;
+    // }
+    console.log("O turn");
+});
