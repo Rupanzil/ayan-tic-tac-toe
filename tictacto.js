d6aa05bf418683d268c16da6c9b164d3e1eff918
Ayan-Alvi <ayanalvi674@gmail.com>
2025-05-04 11:12:02 +0530

    Updated Tic Tac Toe logic

--- tictacto.js
+++ tictacto.js
@@ -2,14 +2,16 @@ let box = document.querySelectorAll(".cell");
 let reset = document.querySelector(".restart");
 let turnO = true;
 
-box.addEventListener("click",function(){
-    // if(turnO){
-    //     console.log("O turn");
-    //     turnO=false;
-    // }
-    // else{
-    //     box.innerText="X";
-    //     trunO=false;
-    // }
-    console.log("O turn");
+box.forEach((cell) => {
+  cell.addEventListener("click", () => {
+    if(turnO){
+        cell.innerText = "O";
+        turnO = false;
+    }   
+    else{
+        cell.innerText = "X";
+        turnO = true;
+    } 
+    cell.disabled = true;
+  });
 });
